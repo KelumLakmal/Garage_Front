211f116fcf371f32b8e00b32e776bba79cc01e91
Kelum Lakmal <smklkarunarathna@gmail.com>
2026-01-16 23:00:02 +0530

    Seventh Commit

--- src/modules/Home.jsx
+++ src/modules/Home.jsx
@@ -1,10 +1,165 @@
-import React from 'react'
+import { Accordion, AccordionDetails, AccordionSummary, Backdrop, Box, Button, Card, CardContent, CardMedia, CircularProgress, Grid, Paper, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import homeImage from "../assets/home1.jpg"
+import vehicleService from '../api/vehicleService';
+import { useEffect } from 'react';
+import { ExpandMore } from '@mui/icons-material';
 
 const Home = () => {
+
+  const [vehicles, setVehicles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [startIndex, setStartIndex] = useState(0);
+
+
+  const fetchVehicles = async (paramObject) => {
+    try {
+      setLoading(true);
+      await new Promise((resolve) => setTimeout(resolve, 0));
+      const vehicles = await vehicleService.getAllVehicles(paramObject);
+      console.log("vehic", vehicles);
+
+      setVehicles(vehicles);
+
+    } catch (err) {
+      console.error("Error", err.message);
+      window.alert(`You have following error: ${err.message}`);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchVehicles();
+  }, []);
+
+  useEffect(() => {
+    if (!vehicles.length) return;
+
+    const interval = setInterval(() => {
+      console.log("Timer");
+
+      setStartIndex((prev) => (prev + 4) % vehicles.length);
+    }, 10000);
+
+    return () => clearInterval(interval);
+
+  }, [vehicles]);
+
+  // const visibleVehicles = (startIndex + 4) <= vehicles.length
+  //   ? vehicles.slice(startIndex, startIndex + 4)
+  //   : [
+  //     ...vehicles.slice(startIndex),
+  //     ...vehicles.slice(0, (startIndex + 4) % vehicles.length)
+  //   ];
+  // console.log(visibleVehicles);
+  const visibleVehicles = vehicles.slice(startIndex, startIndex + 4);
+
+
+
   return (
-   <>
-    <h2>HOme Pageee...</h2>
-   </>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <Backdrop
+          sx={(theme) => ({ color: "#fff", zIndex: theme.zIndex.drawer + 1 })}
+          open={loading}
+        >
+          <CircularProgress color='inherit' />
+        </Backdrop>
+        <Grid container rowSpacing={1} columnSpacing={1}>
+          <Grid size={{ md: 12, xs: 12 }}>
+            <Box sx={{
+              display: "flex",
+              gap: 2,
+              height: "80vh"
+            }}>
+              <Box sx={{ flex: 3 }}>
+                <Box
+                  component="img"
+                  alt='Home Image'
+                  src={homeImage}
+                  sx={{
+                    width: "100%",
+                    // maxHeight: "400px",
+                    // height: "500px",
+                    height: "80vh",
+                    objectFit: "cover",
+                    borderRadius: 5
+                  }}
+                />
+
+              </Box>
+              <Box sx={{ flex: 1, overflowY: "auto" }}>
+                {/* card */}
+
+                {visibleVehicles.slice(0, 4).map((v) => {
+                  return (v.imageUrl &&
+                    <Card key={v.id} sx={{
+                      mb: 2,
+                      width: "100%",
+                      transform: "scale(1)",
+                      "&:hover": {
+                        transform: "scale(1.05)"
+                      },
+                      transition: "transform 0.3s linear"
+                    }} >
+                      <CardMedia
+                        sx={{ height: 150, }}
+                        image={v?.imageUrl}
+                      />
+                      <CardContent>
+                        <Typography variant="body2" color='text.secondary' component="div">Latest Repairs</Typography>
+                      </CardContent>
+                    </Card>
+                  )
+                }
+                )}
+              </Box>
+
+            </Box>
+          </Grid>
+
+          <Grid size={{ md: 12, xs: 6 }}>
+
+            <Box>
+              <Accordion sx={{background: 'linear-gradient(90deg, #d4d5ddff, #ffffffff)'}}>
+                <AccordionSummary
+                  expandIcon={<ExpandMore />}
+                >
+                  <Typography variant='body1' >
+                    More informations
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Box sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    fontSize: 14,
+                    color: 'text.secondary',
+                    gap: 1
+                  }}>
+                    <Typography sx={{
+                      fontSize: 14
+                    }}
+                      color='text.secondary'
+                    >
+                      Email: vdvgvxgx.mail.com
+                    </Typography>
+
+                    <Typography sx={{ fontSize: 14}}>
+                      Contact No: 06746490xs
+                    </Typography>
+                   
+                  </Box>
+
+                </AccordionDetails>
+              </Accordion>
+            </Box>
+          </Grid>
+        </Grid>
+
+      </Box>
+    </>
   )
 }
 
--- src/modules/Mainwindow.jsx
+++ src/modules/Mainwindow.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Mainwindow.css";
 import { Outlet, Link } from "react-router-dom";
 
@@ -17,12 +17,33 @@ import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
 import { useAuth } from "../auth/AuthContext";
+import PersonIcon from '@mui/icons-material/Person';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { Avatar, IconButton, Menu, MenuItem } from "@mui/material";
+import { AccountCircle, AccountCircleOutlined, AccountCircleRounded, AccountCircleSharp, AccountCircleTwoTone, PersonPinCircle } from "@mui/icons-material";
+import userLoginImg from "../assets/userLogin.jpg"
 
 const drawerWidth = 240;
 
 const Mainwindow = () => {
 
-  const { hasPermission } = useAuth();
+  const { hasPermission, logOut, user } = useAuth();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const [showWelcome, setShowWelcome] = useState(true);
+
+  const handleMenuOpen = (event) => {
+    // console.log("EVENT", event.currentTarget);
+    setAnchorEl(event.currentTarget);
+  }
+  const hanldeMenuClose = () => {
+    setAnchorEl(null);
+  }
+  const getUserName = () => {
+    return user.userName.charAt(0) + user.userName.slice(1);
+  }
 
   const MENU_ITEMS = [
     { label: "Home", path: "/main/home", permission: null },
@@ -34,8 +55,26 @@ const Mainwindow = () => {
   // const filterMenus = () => {
   //   return MENU_ITEMS.filter((m) => !m.permission || hasPermission(m.permission));
   // }
+  const userMenus = (
+    <Menu
+      anchorEl={anchorEl}
+      open={open}
+      onClose={hanldeMenuClose}
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "right"
+      }}
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right"
+      }}
 
-  
+    >
+      <MenuItem onClick={hanldeMenuClose}> <ListItemIcon><PersonIcon /></ListItemIcon>Profile</MenuItem>
+      <MenuItem onClick={() => { hanldeMenuClose(); logOut() }}><LogoutIcon sx={{ fontSize: 20, mr: 2 }} /> Log out</MenuItem>
+    </Menu>
+  );
+
 
   return (
     <>
@@ -43,13 +82,45 @@ const Mainwindow = () => {
         <CssBaseline />
         <AppBar
           position="fixed"
-          sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        // sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, background: 'linear-gradient(120deg, #1d76a2ff, #b7dcefff)' }}
+          // sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          // className="app-bar"
+          sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, backgroundImage: "linear-gradient(120deg, rgba(27, 39, 181, 1), rgba(72, 85, 228, 1)) " }}
         >
           <Toolbar>
-            <Typography variant="h6" noWrap component="div">
-              Welcome Back...
-            </Typography>
+            <Box sx={{ flexGrow: 1 }} >
+              {showWelcome && (
+                <Typography
+                  variant="h6"
+                  noWrap
+                  component="div"
+                  // className="welcome"
+                  // sx={{ background: 'red' }}
+                  // onAnimationEnd={() => setShowWelcome(false)}
+                  >
+                  Welcome Back...
+                </Typography>
+
+              )}
+            </Box>
+            <Box sx={{
+              display: "flex",
+              alignItems: "center",
+              // gap: 1
+            }}>
+              <IconButton
+                size="large"
+                color="inherit"
+                edge="start"
+                aria-haspopup="true"
+                onClick={handleMenuOpen}
+              >
+                {/* <AccountCircle sx={{ fontSize: 28 }} /> */}
+                <Avatar alt="user icon" sx={{ width: 30, height: 30 }} src={userLoginImg} />
+              </IconButton>
+              {/* <Typography variant="body1">{user.userName.charAt(0).toUpperCase() + user.userName.slice(1)}</Typography> */}
+              <Typography variant="body1">{user.firstName + " " + user.lastName}</Typography>
+              {userMenus}
+            </Box>
           </Toolbar>
         </AppBar>
         <Drawer
@@ -69,12 +140,12 @@ const Mainwindow = () => {
               {MENU_ITEMS.filter((m) => !m.permission || hasPermission(m.permission)).map((menu, index) => (
                 <ListItem key={index} disablePadding>
                   <ListItemButton component={Link} to={menu.path}>
-                    <ListItemIcon>{index % 2 == 0 ? <InboxIcon/> : <MailIcon/>}</ListItemIcon>
-                    <ListItemText primary={menu.label}/>
+                    <ListItemIcon>{index % 2 == 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                    <ListItemText primary={menu.label} />
                   </ListItemButton>
                 </ListItem>
               ))}
-              
+
               {/* <ListItem disablePadding>
                 <ListItemButton component={Link} to="/main/home">
                     <ListItemIcon>
